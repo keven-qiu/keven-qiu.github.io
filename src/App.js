046ba3ac474e6b3d8f46bd4b2dd1f9c8d35ac93d
Keven Qiu <qiukeven@icloud.com>
2024-05-05 15:23:59 -0600

    react change

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header>
+        <title>Keven Qiu</title>
+        <meta name="google-site-verification" content="21eJbv5WixaWclak6MvA3esxatoOP1BCPBNKCi37bYI"/>
+    </header>
+    <body>
+        <div id="root">
+            <div id="intro">
+                <h1>Keven Qiu</h1>
+                <a href="https://www.linkedin.com/in/keven-qiu/">LinkedIn</a> <a href="https://github.com/keven-qiu">GitHub</a>
+                <p>I am currently a 4th year undergraduate student double majoring in Computer Science and Combinatorics & Optimization at the <a href="https://uwaterloo.ca/">University of Waterloo</a>.
+                    <br />
+                    <br />
+                    My main academic interests are in algorithms and discrete mathematics, in particular, combinatorial optimization. I am also interested in artificial intelligence, machine learning, and neural networks.
+                    <br />
+                    <br />
+                    Email: keven.qiu@uwaterloo.ca
+                </p>
+            </div>
+            <div id="work">
+                <h2>Work Experience/Research</h2>
+                <ul>
+                    <li>Undergraduate Research Assistant at University of Waterloo, Department of Combinatorics & Optimization, under Dr. Joseph Cheriyan, funded by MURA (Fall 2024)</li>
+                    <li>Software Development Intern at <a href="https://www.calian.com/service-lines/advanced-technologies/">Calian, Advanced Technologies</a> (Spring 2024)</li>
+                    <li>Software Engineering Intern at <a href="https://www.augmenta.ai/">Augmenta AI</a> (Fall 2023)</li>
+                    <li>Undergraduate Research Assistant at <a href="https://mccaig.ucalgary.ca/">McCaig Institute of Bone and Joint Health, University of Calgary, under Dr. Sarah Manske</a> (Winter 2023)</li>
+                    <li>Software Development Intern at <a href="https://www.calian.com/service-lines/advanced-technologies/">Calian, Advanced Technologies</a> [Inmarsat GX6] (Spring 2022)</li>
+                    <li>Software Development Intern at <a href="https://www.calian.com/service-lines/advanced-technologies/">Calian, Advanced Technologies</a> [Inmarsat GX5] (Fall 2021)</li>
+                    <li>Mathematics Tutor at <a href="https://www.kumon.com/ca-en/">Kumon, North America</a></li>
+                </ul>
+            </div>
+            <div id="research">
+                <h2>Publications and Abstracts</h2>
+                <ul>
+                    <li>Waungana, T.H., Qiu, K., Tse, J.T., Anderson, D.D., Emery, C.A., Boyd, S.K., Manske, S.L. (2023). Accuracy of volumetric bone mineral density measurement in weight bearing, cone beam computed tomography. Journal of Clinical Densitometry. (Submitted)</li>
+                    <li>Tadiwa H. Waungana, Keven Qiu, Justin T. Tse, Donald D. Anderson, Sarah L. Manske. Accuracy of BMD measurement in weight bearing, cone-beam CT. The American Society for Bone and Mineral Research, 2023. Vancouver, BC, Canada.</li>
+                </ul>
+            </div>
+            <div id="presentations">
+                <h2>Presentations</h2>
+                <ul>
+                    <li>Augmenta Reading Group - Combinatorial Optimization.</li>
+                    <li>End of Undergraduate Research Term at UCalgary - BMD Measurement with Cone Beam, Weight-Bearing CT and Reconstruction Algorithms. <a href="https://keven-qiu.github.io/presentations/End of Term Presentation.pptx">[PPTX]</a></li>
+                </ul>
+            </div>
+            <div id="notes">
+                <h2>Personal Course Notes</h2>
+                <h3>General Math/Statistics</h3>
+                <ul>
+                    <li><a href="https://keven-qiu.github.io/notes/MATH235.pdf">MATH 235: Linear Algebra 2 for Honours Mathematics</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/MATH237.pdf">MATH 237: Calculus 3 for Honours Mathematics</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/MATH239.pdf">MATH 239: Introduction to Combinatorics</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/STAT231.pdf">STAT 231: Statistics</a></li>
+                </ul>
+                <h3>Computer Science</h3>
+                <ul>
+                    <li><a href="https://keven-qiu.github.io/notes/CS341.pdf">CS 341: Algorithms</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CS350.pdf">CS 350: Operating Systems</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CS365.pdf">CS 365: Models of Computation (Advanced)</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CS370.pdf">CS 370: Numerical Computation</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CS466_666.pdf">CS 466/666: Algorithm Design and Analysis</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CS480_680.pdf">CS 480/680: Introduction to Machine Learning</a></li>
+                </ul>
+                <h3>Combinatorics & Optimization</h3>
+                <ul>
+                    <li><a href="https://keven-qiu.github.io/notes/CO250.pdf">CO 250: Introduction to Optimization</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CO330.pdf">CO 330: Combinatorial Enumeration</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CO342.pdf">CO 342: Introduction to Graph Theory</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CO351.pdf">CO 351: Network Flow Theory</a></li>
+                    <li><a href="https://keven-qiu.github.io/notes/CO450_650.pdf">CO 450/650: Combinatorial Optimization</a></li>
+                </ul>
+                <h3>Electives</h3>
+                <ul>
+                    <li><a href="https://keven-qiu.github.io/notes/ECON102.pdf">ECON 102: Introduction to Macroeconomics</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+    </div>
+  );
+}
+
+export default App;
