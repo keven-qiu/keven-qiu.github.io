245109e61731edf3719f93bcc6e6980fc614d74d
Keven Qiu <qiukeven@icloud.com>
2024-09-18 11:28:50 -0400

    Updates

---
+++ precache-manifest.6ad18889a2c04875f6b226f5d60f7f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a32439841148facffba92342eed4f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30ef4240e1a6f550c483",
+    "url": "/static/css/main.88c868f1.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "30ef4240e1a6f550c483",
+    "url": "/static/js/main.d8a0c57d.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
