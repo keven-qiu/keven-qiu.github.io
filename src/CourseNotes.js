54a1c23cb12678b03089aa1bd2b9943f655069b5
Keven Qiu <qiukeven@icloud.com>
2025-01-13 14:40:29 -0500

    update

--- src/CourseNotes.js
+++ src/CourseNotes.js
@@ -20,6 +20,7 @@ const CourseNotes = () => {
                 <li><a href={process.env.PUBLIC_URL + '/notes/CS370.pdf'}>CS 370: Numerical Computation</a></li>
                 <li><a href={process.env.PUBLIC_URL + '/notes/CS466_666.pdf'}>CS 466/666: Algorithm Design and Analysis</a></li>
                 <li><a href={process.env.PUBLIC_URL + '/notes/CS480_680.pdf'}>CS 480/680: Introduction to Machine Learning</a></li>
+                <li><a href={process.env.PUBLIC_URL + '/notes/CS487_687.pdf'}>CS 480/680: Introduction to Symbolic Computation</a></li>
             </ul>
             <h3>Combinatorics & Optimization</h3>
             <ul>
@@ -27,6 +28,7 @@ const CourseNotes = () => {
                 <li><a href={process.env.PUBLIC_URL + '/notes/CO330.pdf'}>CO 330: Combinatorial Enumeration</a></li>
                 <li><a href={process.env.PUBLIC_URL + '/notes/CO342.pdf'}>CO 342: Introduction to Graph Theory</a></li>
                 <li><a href={process.env.PUBLIC_URL + '/notes/CO351.pdf'}>CO 351: Network Flow Theory</a></li>
+                <li><a href={process.env.PUBLIC_URL + '/notes/CO444_644.pdf'}>CO 444/644: Algebraic Graph Theory</a></li>
                 <li><a href={process.env.PUBLIC_URL + '/notes/CO450_650.pdf'}>CO 450/650: Combinatorial Optimization</a></li>
             </ul>
             <h3>Electives</h3>
