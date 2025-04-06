f6ac147ef215f3f0caeb5ca71cf4a1e81415c026
Keven Qiu <qiukeven@icloud.com>
2025-04-05 21:55:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -14,19 +14,20 @@ function App() {
         <div className="intro">
             <h1>Keven Qiu</h1>
             <a href="https://www.linkedin.com/in/keven-qiu/">LinkedIn</a> <a href="https://github.com/keven-qiu">GitHub</a>
-            <p>I am a 4th year undergraduate student double majoring in Computer Science and Combinatorics & Optimization at the <a href="https://uwaterloo.ca/">University of Waterloo</a>.
+            <p>
+                I am a recent graduate, double majoring in Computer Science and Combinatorics & Optimization, from the University of Waterloo. I will be joining the Algorithmics group at the University of Alberta for Masters Fall 2025, cosupervised by <a href="https://webdocs.cs.ualberta.ca/~mreza/">Mohammad R. Salavatipour</a> and <a href="https://friggstad.github.io/">Zachary Friggstad</a>.
                 <br />
                 <br />
-                My main academic interests are in algorithms, discrete mathematics, and optimization. I will be undergoing research in designing and analyzing approximation algorithms for combinatorial optimization problems. I am also interested in artificial intelligence, machine learning, and neural networks.
+                My main academic interests are in algorithms, discrete mathematics, and optimization. I am focused on the design and analysis of approximation algorithms.
                 <br />
                 <br />
                 Email: keven.qiu@uwaterloo.ca
             </p>
         </div>
         <div className="work">
-            <h2>Work Experience/Research</h2>
+            <h2>Previous Experience</h2>
             <ul>
-                <li>Undergraduate Research Assistant at University of Waterloo, Department of Combinatorics & Optimization, under Dr. Joseph Cheriyan, funded by the MURA (Fall 2024)</li>
+                <li>Undergraduate Research Assistant at University of Waterloo, Department of Combinatorics & Optimization, under Dr. Joseph Cheriyan. <a href="https://github.com/keven-qiu/PrimalDualSmallCuts">[Github]</a></li>
                 <li>Software Development Intern at <a href="https://www.calian.com/service-lines/advanced-technologies/">Calian, Advanced Technologies</a> (Spring 2024)</li>
                 <li>Software Engineering Intern at <a href="https://www.augmenta.ai/">Augmenta AI</a> (Fall 2023)</li>
                 <li>Undergraduate Research Assistant at University of Calgary, <a href="https://mccaig.ucalgary.ca/">McCaig Institute of Bone and Joint Health</a>, under Dr. Sarah Manske (Winter 2023)</li>
--- src/Presentation.js
+++ src/Presentation.js
@@ -5,8 +5,9 @@ const Presentation = () => {
         <>
             <h2>Presentations</h2>
             <ul>
+                <li>End of Undergraduate Research Term at UCalgary - BMD Measurement with Cone Beam, Weight-Bearing CT and Reconstruction Algorithms. <a href={process.env.PUBLIC_URL + '/presentations/End of Term Presentation.pptx'}>[slides]</a></li>
+                <li>Largest Eigenvalue of Sparase Random Graphs <a href={process.env.PUBLIC_URL + '/presentations/LargestEigenvalue.pdf'}>[slides]</a></li>
                 <li>Augmenta Reading Group - Combinatorial Optimization.</li>
-                <li>End of Undergraduate Research Term at UCalgary - BMD Measurement with Cone Beam, Weight-Bearing CT and Reconstruction Algorithms. <a href={process.env.PUBLIC_URL + '/presentations/End of Term Presentation.pptx'}>[PPTX]</a></li>
             </ul>
         </>
     )
