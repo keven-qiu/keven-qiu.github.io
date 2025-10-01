8cec28263e3445bdc38d809911e61f182aad8909
Keven Qiu <qiukeven@icloud.com>
2025-10-01 13:48:35 -0600

    update

--- src/CourseNotes.js
+++ src/CourseNotes.js
@@ -6,7 +6,7 @@ const CourseNotes = () => {
             <h2>Notes</h2>
             <h3>University of Alberta</h3>
             <ul>
-                <li><a href={process.env.PUBLIC_URL}>CMPUT 501: Advanced Algorithms</a></li>
+                <li><a href={'https://webdocs.cs.ualberta.ca/~mreza/courses/AdvancedF25/index.html'}>CMPUT 501: Advanced Algorithms</a></li>
                 <li><a href={process.env.PUBLIC_URL + '/notes/CMPUT605.pdf'}>CMPUT 605: Approximation Algorithms</a></li>
             </ul>
             <h3>University of Waterloo</h3>
