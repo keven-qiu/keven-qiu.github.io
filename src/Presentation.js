77a630ec75f27389bded1cb88bf75bea15ebd9d4
Keven Qiu <qiukeven@icloud.com>
2025-12-23 02:04:46 -0600

    update

--- src/Presentation.js
+++ src/Presentation.js
@@ -5,6 +5,7 @@ const Presentation = () => {
         <>
             <h2>Presentations</h2>
             <ul>
+                <li>An O(log n)-Approximation Algorithm for (p,q)-Flexible Graph Connectivity via Independent Rounding <a href={process.env.PUBLIC_URL + '/presentations/log-n-pqfgc-presentation.pdf'}>[slides]</a></li>
                 <li>Largest Eigenvalue of Sparse Random Graphs <a href={process.env.PUBLIC_URL + '/presentations/LargestEigenvalue.pdf'}>[slides]</a></li>
                 <li>End of Undergraduate Research Term at UCalgary - BMD Measurement with Cone Beam, Weight-Bearing CT and Reconstruction Algorithms. <a href={process.env.PUBLIC_URL + '/presentations/End of Term Presentation.pptx'}>[slides]</a></li>
                 <li>Augmenta Reading Group - Combinatorial Optimization.</li>
